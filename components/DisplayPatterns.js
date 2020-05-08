500beb8731eb4229b3bccba99bb818c3f2fc0a41
Anton <anton@oas.nu>
2020-05-08 17:03:56 +0200

    DEV: Download svg blod begun

--- components/DisplayPatterns.js
+++ components/DisplayPatterns.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from '@emotion/styled'
 import { preparePath } from './MapDrawer'
 
@@ -29,6 +29,7 @@ const Item = styled('li')`
     border-radius: 15px;
     background-color: var(--secondary-bg-color);
     box-shadow: var(--main-box-shadow);
+    cursor: pointer;
   }
 
   svg {
@@ -45,67 +46,125 @@ const Item = styled('li')`
   # Componet
 ==============================================================================*/
 
-const Pattern = ({ path }) => {
+class Pattern extends Component {
 
-  // Decalre variables
-  let updatedPath = null
-  let sX = 0,
-      sY = 0,
-      lX = 0,
-      lY = 0,
-      addX = 0,
-      addY = 0
-
-  // Find largest and smallest value for x and y
-  // Used to set svg viewbox
-  path.forEach(s => {
-    const { x, y } = s
-    sX = x < sX ? x : sX 
-    sY = y < sY ? y : sY
-    lX = x > lX ? x : lX 
-    lY = y > lY ? y : lY
-  })
-
-  // If smallest X is smaller than 0, recalculate viewbox values
-  if ( sX < 0 ) {
-    addX = sX*-1
-    sX = sX + addX
-    lX = lX + addX
+  constructor(props) {
+    super(props);
+    this.ref = React.createRef();
   }
 
-  // If smallest Y is smaller than 0, recalculate viewbox values
-  if ( sY < 0 ) {
-    addY = sY*-1
-    sY = sY + addY
-    lY = lY + addY
+  state = {
+    svg: null
   }
 
-  // If the viewbox was recalculated, also relocate all paths to fit inside the new viewbox
-  if ( addY > 0 || addX > 0 ) {
-    // Return new object/array so that changes dosent effect other parts of the site
-    updatedPath = path.map(s => ({
-      type: s.type,
-      x: s.x + addX,
-      y: s.y + addY
-    }))
+  handleRef = ref => {
+    this.ref = ref
+    this.prepareSVG()
   }
 
-  // Make sure that the viewbox always has a width and height
-  lX = lX > 0 ? lX : 1
-  lY = lY > 0 ? lY : 1
+  prepareSVG = () => {
+    if ( this.state.svg ) return
+    let { path } = this.props
+    
+    // Declare variables
+    let updatedPath = null
+    let sX = 0,
+        sY = 0,
+        lX = 0,
+        lY = 0,
+        addX = 0,
+        addY = 0
 
-  // Update path is size was recalculated
-  let finalPath = updatedPath ? updatedPath : path
+    // Find largest and smallest value for x and y
+    // Used to set svg viewbox
+    path.forEach(s => {
+      const { x, y } = s
+      sX = x < sX ? x : sX 
+      sY = y < sY ? y : sY
+      lX = x > lX ? x : lX 
+      lY = y > lY ? y : lY
+    })
 
-  return (
-    <Item>
-      <div className="inner">
-        <svg viewBox={`${sX} ${sY} ${lX} ${lY}`}>
-          <path stroke="#000" strokeWidth="1" fill="none" d={preparePath(finalPath)} />
-        </svg>
-      </div>
-    </Item>
-  )
+    // If smallest X is smaller than 0, recalculate viewbox values
+    if ( sX < 0 ) {
+      addX = sX*-1
+      sX = sX + addX
+      lX = lX + addX
+    }
+
+    // If smallest Y is smaller than 0, recalculate viewbox values
+    if ( sY < 0 ) {
+      addY = sY*-1
+      sY = sY + addY
+      lY = lY + addY
+    }
+
+    // If the viewbox was recalculated, also relocate all paths to fit inside the new viewbox
+    if ( addY > 0 || addX > 0 ) {
+      // Return new object/array so that changes dosent effect other parts of the site
+      updatedPath = path.map(s => ({
+        type: s.type,
+        x: s.x + addX,
+        y: s.y + addY
+      }))
+    }
+
+    // Make sure that the viewbox always has a width and height
+    lX = lX > 0 ? lX : 1
+    lY = lY > 0 ? lY : 1
+
+    // Update path is size was recalculated
+    let finalPath = updatedPath ? updatedPath : path
+
+
+    // Create SVG element
+    let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg')
+    svg.setAttributeNS('http://www.w3.org/2000/xmlns/', 'xmlns:xlink', 'http://www.w3.org/1999/xlink')
+    svg.setAttributeNS(null, 'viewBox', `${sX} ${sY} ${lX} ${lY}`)
+
+    let svgPath = document.createElementNS('http://www.w3.org/2000/svg','path')
+    svgPath.setAttributeNS(null, 'stroke', '#000')
+    svgPath.setAttributeNS(null, 'stroke-width', '1')
+    svgPath.setAttributeNS(null, 'fill', 'none')
+    svgPath.setAttributeNS(null, 'd', preparePath(finalPath))
+
+    svg.appendChild(svgPath)
+
+    this.setState({
+      svg: svg
+    }, () => {
+      this.ref.appendChild( svg )
+    })
+  }
+
+  download = () => {
+    const { svg } = this.state
+    if ( !svg ) return
+
+    var a = window.document.createElement('a');
+    a.href = window.URL.createObjectURL(new Blob([this.ref.innerHTML], {type: 'image/svg+xml'}));
+    a.download = 'pattern.svg';
+
+    // Append anchor to body.
+    document.body.appendChild(a);
+    a.click();
+
+    // Remove anchor from body
+    document.body.removeChild(a);
+  }
+
+  render () {
+    return (
+      <Item>
+        <div 
+          className="inner"
+          title="download"
+          onClick={this.download()}
+          ref={ref => this.handleRef(ref)} 
+        />
+      </Item>
+    )
+  }
 }
 
 const DisplayPatterns = ({ paths }) => (
